2fe3bcc0ed77701751cf5004564f989a7dc1eb5e
cep104 <candicelizabethpeters@gmail.com>
2021-03-26 10:43:01 -0500

    files connected

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "show_cli_group4/version"
+# require_relative "show_cli_group4/version"
 
 require "bundler"
 Bundler.require
+
+require_rel './show_cli_group4'
 
