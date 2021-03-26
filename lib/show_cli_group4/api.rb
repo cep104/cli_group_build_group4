7a4b1fd8ff781544cbcca581908b09a1bd644c6d
cep104 <candicelizabethpeters@gmail.com>
2021-03-26 10:59:18 -0500

    made object

--- lib/show_cli_group4/api.rb
+++ lib/show_cli_group4/api.rb
@@ -1,3 +1,12 @@
 class API 
-    # binding.pry
+   def self.get_data 
+    response = RestClient.get('http://api.tvmaze.com/search/shows?q=friends')
+    shows = JSON.parse(response)
+    shows.each do |show|
+        new_show = show["show"]
+    Show.new(new_show["name"], new_show["genres"], new_show["rating"])
+    
+   end
+#    binding.pry
+   end
 end
--- lib/show_cli_group4/show.rb
+++ lib/show_cli_group4/show.rb
@@ -1,3 +1,18 @@
 class Show 
-    
+    attr_accessor :name, :genres, :rating 
+    @@all = []
+    def initialize(name, genres, rating)
+        @name = name 
+        @genres = genres 
+        @rating = rating 
+        save
+    end
+
+    def save 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
